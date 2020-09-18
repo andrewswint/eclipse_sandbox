91adfec97fd3d21e6941bc3c1ca60fc9f34eb4f2
andrewswint <andrewswint1976@gmail.com>
2020-09-18 00:28:08 -0500

    second problem of the night. kind of a free be

---
+++ src/hackranker/javaSubstring.java
@@ -0,0 +1,17 @@
+package hackranker;
+
+public class javaSubstring {
+
+	public static void main(String[] args) {
+
+		String string = "helloworld";
+		int start = 3;
+		int end = 7;
+		
+		String output = string.substring(start, end);
+		
+		System.out.println(output);
+		
+	}
+
+}
